3a7c9ee2c68d3a51f9e644a7ac25ab6495480470
Jinyers <jinyerru@gmail.com>
2025-06-29 11:44:54 +0300

    [Guessing game] Added loop

--- c2-guessing-game/src/main.rs
+++ c2-guessing-game/src/main.rs
@@ -9,24 +9,32 @@ fn main() {
 
     println!("Secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    let mut guess: String;
+    let mut guess_number: u32;
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        guess = String::new();
 
-    let guess: u32 = guess
-        .trim()
-        .parse()
-        .expect("Please type a number!");
-
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        guess_number = guess
+            .trim()
+            .parse()
+            .expect("Please type a number!");
+    
+        println!("You guessed: {}", guess_number);
+    
+        match guess_number.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
     }
 }
